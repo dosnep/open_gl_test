1132370ab17dfdae6916cac580393e767f0919a2
pierre.dosne <pierre.dosne@thalesgroup.com>
2021-01-11 12:11:42 +0100

    init python start kit

---
+++ python_package/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+        name="project",
+    version="0.0.1",
+    keywords=["project"],
+    install_requires=[
+        "Flask==1.1.2"
+    ],    
+    packages=setuptools.find_packages(),
+)
+
